9c234749935b488ed5272cd478ea2ea26c6507c0
Dennis Lee <v-dele@microsoft.com>
2019-11-27 22:39:01 -0800

    Changes to get the quote puller working.

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import os
-
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
-
-    return facts[0].getText()
-
+    fact = soup.find("div", id="content")
+    return fact.getText()
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    return get_fact()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6787))
+    port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
