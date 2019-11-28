a3a638ac3838f43f3819a05301705cbdb94092e5
Dennis Lee <v-dele@microsoft.com>
2019-11-28 01:30:49 -0800

    Added PigLatin code. Still need to figure out why Location isn't in the header response.

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ def get_fact():
     fact = soup.find("div", id="content")
     return fact.getText()
 
+def get_piglatin(input):
+    request_url = "http://hidden-journey-62459.herokuapp.com/piglatinize/"
+    param_data = {"input_text": input}
+    response = requests.post(request_url, param_data)
+    # result = response.headers['Location']
+    result = "<ul>"
+    result += "<p>"+param_data["input_text"]+"</p>"
+    for i in response.headers:
+        result += "<li>" + i + " | " + response.headers[i] + "</li>"
+    result += "<ul>"
+    result += "<p>" + response.text + "</p>"
+    result += "<p>Is redirect: " + str(response.is_redirect) + "</p>"
+    return result
+
 @app.route('/')
 def home():
-    return get_fact()
+    return get_piglatin(get_fact())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
